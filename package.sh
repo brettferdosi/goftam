d252bf139d03f8a5abea452da80586dd0e95bbdd
Brett Gutstein <brett@normative.cc>
2023-03-27 22:36:21 -0400

    replace package.sh with make

--- package.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# first argument is path to the .app file
-# second argument is path of the directory the .pkg will go in
-
-# pkg seems to have trouble installing to the user's home directory so install for all users
-productbuild --component "$1" "/Library/Input Methods/" "$2/GoftamInstaller.pkg"
